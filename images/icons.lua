333a6ecf2f591a06c71cf752e8281464364a773f
cardcash <terencecawleynew@gmail.com>
2026-06-11 15:18:13 +0800

    Update icon asset IDs in icons.lua

--- images/icons.lua
+++ images/icons.lua
@@ -4,10 +4,10 @@ local Icons = {}
 -- Use these slots to store the rbxassetid strings for your pixel art.
 -- You can upload your own icons to Roblox and paste the IDs here.
 Icons.Registry = {
-    Close = "rbxassetid://0000000000",
-    Minimize = "rbxassetid://0000000000",
-    Maximize = "rbxassetid://0000000000",
-    Folder = "rbxassetid://0000000000",
+    Close = "rbxassetid://79586827082940",
+    Minimize = "rbxassetid://119976625630400",
+    Maximize = "rbxassetid://136359168487680",
+    Folder = "rbxassetid://99356092113217",
     Settings = "rbxassetid://0000000000",
     Check = "rbxassetid://0000000000",
     Warning = "rbxassetid://0000000000"
